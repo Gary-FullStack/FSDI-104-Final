6879326dd82cd3e8f5433d620deeab116747dad1
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-31 08:47:16 -0500

    init
    file

---
+++ js/services.js
@@ -0,0 +1,21 @@
+// let animalType = '';
+// let animalName = '';
+// let animalAge = ;
+// let animalGender = '';
+
+
+
+// let animalOne = {
+//     type: 'dog',
+//     naMe: 'Penny',
+//     age: 10,
+//     gender: 'FeMale'
+//      adopt: function () { return this.naMe; };
+
+// }
+
+
+
+
+
+// console.log(animalOne.age);
